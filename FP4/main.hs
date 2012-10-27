98c1d6374f0a7014f7185fb3f7d869ac64787e83
Garry Sharp <0801585s@bo620-29u.(none)>
2012-10-27 22:12:24 +0100

    allows begin statements with begin statements, functing code

--- FP4/main.hs
+++ FP4/main.hs
@@ -1,9 +1,10 @@
 module Main where
+import Data.List
 import Data.List.Split
 
 
---data Var = String
---data Decl = MakeDecl [Var] deriving (Read, Show)
+data Var = MakeVar String deriving (Read, Show)
+data Decl = MakeDecl String deriving (Read, Show)
 
 data IntExp
   	= IVar String
@@ -13,61 +14,82 @@ data IntExp
   	| Mul IntExp IntExp
   	| Div IntExp IntExp
   	deriving (Read, Show)
-{-
+
 data BoolExp
-	 = LT IntExp IntExp
-	 | EQ IntExp IntExp
-	 | GT IntExp IntExp
+	 = ILT IntExp IntExp
+	 | ILTEQ IntExp IntExp
+	 | IEQ IntExp IntExp
+	 | IGT IntExp IntExp
+	 | IGTEQ IntExp IntExp
 	 deriving (Read, Show)
 
 data Stmt
-	 = Begin [Decl] [Stmt]
+	 = Begin String [Stmt]	-- Change String to Decl
 	 | Assign Var IntExp
 	 | Read Var IntExp
 	 | Write IntExp
+	 | While BoolExp [Stmt]		-- Orginially a single statement (a begin one I Imagine)
+	 deriving (Read, Show) 
+{-
 	 | IfThenElse BoolExp Stmt Stmt
 	 | While BoolExp Stmt
-	 deriving (Read, Show) -}
-
+	 deriving (Read, Show) 
+-}
 
 -- ################################################ --
 --                 Tree Builder                     --
 -- ################################################ --
-{-
-treeBuilder :: [Tokens] -> Stmt
-treeBuilder ((I a):xs) =  
+-- ******************* --
+-- Building up Bool --
+-- ******************* --
 
-keyWordEval :: String -> [Tokens] -> Stmt
-ketWordEval "begin" (x:xs) = Begin x (treeBuilder xs)
+boolExpEval :: [Tokens] -> BoolExp -- Boolean Expressions must be between parenthesis. 2 IntExp's separated by a BooleanOperator
+boolExpEval (_:xs) =
+	boolExpEvalHelper (getBoolArgsHelper2 xs) (intExpEval (getBoolArgs xs)) (intExpEval (getParensArgs(drop ((getBoolArgsHelper xs) + 1) xs)))
 
-baseIntExpEval :: Tokens -> IntExp
-baseIntExpEval (Identifier x) = (Ivar x)
-baseIntExpEval (Number x) = (Icon x)
-
-opIntExpEval Token -> IntExp -> IntExp
-opIntExpEval (MathematicalOperator "*") x y = Mul x y
-opIntExpEval (MathematicalOperator "-") x y = Sub x y
-opIntExpEval (MathematicalOperator "/") x y = Div x y
-opIntExpEval (MathematicalOperator "+") x y = Add x y
--}
+boolExpEvalHelper :: String -> IntExp -> IntExp -> BoolExp
+boolExpEvalHelper op x y 
+	| op == "<" = ILT x y
+	| op == "<=" = ILTEQ x y
+	| op == "==" = IEQ x y
+	| op == ">=" = IGTEQ x y
+	| op == ">" = IGT x y
 
 
-{-
-stateMentBuilder :: [Tokens] -> [Stmt]
-stateMentBuilder ((Keyword "end"):xs) = []
-stateMentBuilder ((Keyword "read"):xs) = (Read (Var (xs!!0)) (IntExpEval (getLineArgs (drop 1 xs))))
-stateMentBuilder ((Keyword "write"):xs) =  (Write (IntExpEval (getLineArgs xs)))
--}
+-- ******************* --
+-- Building up Stmts --
+-- ******************* --
+
+statementBuilder :: [Tokens] -> Stmt
+statementBuilder ((KeyWord "begin"):(Identifier x):xs) =
+	(Begin x (statementArrayBuilder xs))
+
+statementArrayBuilder :: [Tokens] -> [Stmt]
+statementArrayBuilder ((KeyWord "end"):xs) = []
+statementArrayBuilder ((KeyWord "begin"):xs) =
+	(statementBuilder ((KeyWord "begin"):xs)) : (statementArrayBuilder (getArgsFrom (KeyWord "end") xs))
+statementArrayBuilder ((KeyWord "read"): xs) = 				-- For a Read Statement
+	(Read (MakeVar "Garry Var") (intExpEval (getLineArgs (drop 1 xs)))) : statementArrayBuilder (drop ((getLineArgsHelper xs) + 1) xs)
+statementArrayBuilder ((KeyWord "write"):xs) =  			-- For a Write Statement
+	(Write (intExpEval (getLineArgs xs))) : statementArrayBuilder (drop ((getLineArgsHelper xs) + 1) xs)
+statementArrayBuilder ((Identifier x):(AssignmentOperator y):xs) = 	-- For am Assign Operation
+	(Assign (MakeVar x) (intExpEval (getLineArgs xs))) : statementArrayBuilder (drop ((getLineArgsHelper xs) + 1) xs)
 
 
+-- ******************* --
+-- Building up IntExps --
+-- ******************* --
 
+	-- When given a list of tokens (all of which make up an IntExp) will return an IntExp
 intExpEval :: [Tokens] -> IntExp
+intExpEval (x:[]) = iVarOrICon x
 intExpEval (x:xs) 
 	| (head xs) == (MathematicalOperator "*") = intExpEvalHelper (Mul (iVarOrICon x) (iVarOrICon (xs!!1))) (drop 2 xs)
 	| (head xs) == (MathematicalOperator "/") = intExpEvalHelper (Div (iVarOrICon x) (iVarOrICon (xs!!1))) (drop 2 xs)
 	| (head xs) == (MathematicalOperator "-") = intExpEvalHelper (Sub (iVarOrICon x) (iVarOrICon (xs!!1))) (drop 2 xs)
 	| (head xs) == (MathematicalOperator "+") = intExpEvalHelper (Add (iVarOrICon x) (iVarOrICon (xs!!1))) (drop 2 xs)
 
+	-- Helper to the above function (allows for intExp's to be part of a bigger intExp) NB. As of yet this doesn't support order of operations
 intExpEvalHelper :: IntExp -> [Tokens] -> IntExp
 intExpEvalHelper y [] = y
 intExpEvalHelper y ((MathematicalOperator "*"):xs) = intExpEvalHelper (Mul y (iVarOrICon (head xs))) (drop 1 xs)
@@ -75,20 +97,53 @@ intExpEvalHelper y ((MathematicalOperator "/"):xs) = intExpEvalHelper (Div y (iV
 intExpEvalHelper y ((MathematicalOperator "-"):xs) = intExpEvalHelper (Sub y (iVarOrICon (head xs))) (drop 1 xs)
 intExpEvalHelper y ((MathematicalOperator "+"):xs) = intExpEvalHelper (Add y (iVarOrICon (head xs))) (drop 1 xs)
 
+	-- Used for evaluating lead nodes in an intExp tree (determines whether a value is either a number or a variable
 iVarOrICon :: Tokens -> IntExp
 iVarOrICon (Identifier x) = IVar x
 iVarOrICon (Number x) = ICon x
 
-{-
+
+
+-- ******************* --(drop ((getLineArgsHelper xs) + 1) xs)
+--   General Helpers   --
+-- ******************* --
+
+	-- Helper function that returns a small list of tokens (all the tokens up to an end of line character)
 getLineArgs :: [Tokens] -> [Tokens]
 getLineArgs toks =
 	take (getLineArgsHelper toks) toks
 
+	-- Helper function to the above helper function (returns a take index)
 getLineArgsHelper :: [Tokens] -> Int
 getLineArgsHelper (EndOfLine _:_) = 0
-getLineArgsHelper (_:xs) = 1 + getLineArgs xs
+getLineArgsHelper (_:xs) = 1 + getLineArgsHelper xs
 
--}
+getParensArgs :: [Tokens] -> [Tokens]
+getParensArgs toks =
+	take (getParensArgsHelper toks) toks
+
+getParensArgsHelper :: [Tokens] -> Int
+getParensArgsHelper (Parens _:_) = 0
+getParensArgsHelper (_:xs) = 1 + getParensArgsHelper xs
+
+getBoolArgs :: [Tokens] -> [Tokens]
+getBoolArgs toks =
+	take (getBoolArgsHelper toks) toks
+
+getBoolArgsHelper :: [Tokens] -> Int
+getBoolArgsHelper ((BooleanOperator _):_) = 0
+getBoolArgsHelper (_:xs) = 1 + getBoolArgsHelper xs
+
+getBoolArgsHelper2 :: [Tokens] -> String
+getBoolArgsHelper2 ((BooleanOperator x):xs) = x
+getBoolArgsHelper2 (_:xs) = getBoolArgsHelper2 xs
+
+getArgsFrom :: Tokens -> [Tokens] -> [Tokens]
+getArgsFrom y (x:xs) 
+	| x == y = xs
+	| otherwise = (getArgsFrom y xs)
+
+
 
 -- ################################################ --
 --                 Lexical Analyser                 --
@@ -116,8 +171,8 @@ lexicalAnalyser (x:xs)
 	| elem x ["*","-","+","/"] = (MathematicalOperator x) : (lexicalAnalyser xs)						--MathematicalOperater
 	| x == ":" && nextIsEquals (head xs) = (AssignmentOperator (x ++ (head xs))) : (lexicalAnalyser (skip xs))		--AssignmentOperater
 	| x == "=" = (AssignmentOperator x) : (lexicalAnalyser xs)								--AssignmentOperater
-	| x == ";" = (EndOfLine x) : (lexicalAnalyser xs)
-	| x == ")" || x == "(" = (Parens x) : (lexicalAnalyser xs)
+	| x == ";" = (EndOfLine x) : (lexicalAnalyser xs)									--End of Line Char
+	| x == ")" || x == "(" = (Parens x) : (lexicalAnalyser xs)								--Parenthesis
 	| x == "#" =  Comment (unwords (take ((commentDrop xs) - 1) xs)) : lexicalAnalyser (drop (commentDrop xs) xs)		--Comments
 	| elem x (map char2string ['0'..'9']) = (Number (read x :: Int)) : (lexicalAnalyser xs)					--Numbers
 	| otherwise = (Identifier x) : (lexicalAnalyser xs)
@@ -165,7 +220,8 @@ isIntExp str =
     
 main = do
 	 x <- readFile "exampleProg.txt"
-     	 putStr (show (intExpEval (lexicalAnalyser (myDelimiter x))))
+	 let xs = (lexicalAnalyser (myDelimiter x))
+     	 putStr (show (statementBuilder xs))
 
 
 
